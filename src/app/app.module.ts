7602c455833c7111aa76f4e3de21dde639b6ddfb
unknown <maxinbusquedaydesarrollo@gmail.com>
2023-09-07 22:25:13 -0300

    genero componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { AngularComponent } from './componentes/angular/angular.component';
+import { GitComponent } from './componentes/git/git.component';
+import { TerminalesComponent } from './componentes/terminales/terminales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    AngularComponent,
+    GitComponent,
+    TerminalesComponent
   ],
   imports: [
     BrowserModule,
