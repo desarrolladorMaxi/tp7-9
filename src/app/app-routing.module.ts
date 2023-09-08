410f0fdf0ba11b9a4a23083e1758434058030500
unknown <maxinbusquedaydesarrollo@gmail.com>
2023-09-07 22:29:19 -0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { AngularComponent } from './componentes/angular/angular.component';
+import { GitComponent } from './componentes/git/git.component';
+import { TerminalesComponent } from './componentes/terminales/terminales.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component:InicioComponent},
+  {path: 'angular', component:AngularComponent},
+  {path: 'git', component:GitComponent},
+  {path: 'terminales', component:TerminalesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
